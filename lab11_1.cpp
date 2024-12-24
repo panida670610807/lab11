79f6805421651baddef42862990df4180f591ed3
panida670610807 <hiyik757.gmail>
2024-12-24 14:20:14 +0700

    yik

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,21 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    for (int i = 0; i < 3; i++) {
+        cin.get();
+    }
+
+    srand(time(0));
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    string prediction = grades[rand() % 9];
+
+    cout << "\nYou will get " << prediction << " in this 261102." << endl;
+
+        return 0;
+}
